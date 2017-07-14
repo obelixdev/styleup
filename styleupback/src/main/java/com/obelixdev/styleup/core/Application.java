6289939e09bb8ee249368aafa666ebd98aac5ac0
jguerreroc <jguerrero.udea@gmail.com>
2017-07-13 22:27:15 -0500

    Carga inicial del proyecto

---
+++ styleupback/src/main/java/com/obelixdev/styleup/core/Application.java
@@ -0,0 +1,18 @@
+package com.obelixdev.styleup.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+ 
+   public static void main(String[] args) {
+  	    SpringApplication.run(Application.class, args);	  	    
+   }
+   
+  
+   
+}
+
+
